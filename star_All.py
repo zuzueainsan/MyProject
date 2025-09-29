3ad136f812a4fc3ac5d8e81b1b35d33a42564d3e
zuzueainsan <zuzueainsan@gmail.com>
2025-09-29 11:01:07 +0630

    Add files via upload

---
+++ star_All.py
@@ -0,0 +1,31 @@
+for i in range(5):
+    for j in range(i+1):
+        print('*', end=' ')
+    print()
+print("----------------")
+for i in range(5, 0,-1):
+    for j in range(i):
+        print('*', end=' ')
+    print()
+print("----------------")
+for i in range(5):
+    for j in range(1,5-i):
+        print(' ',end=' ')
+    for k in range(i+1):
+        print('*',end=' ')
+    print()
+
+print("----------------")
+for i in range(5):
+    for j in range(1,5-i):
+        print(' ',end='')
+    for k in range(i+1):
+        print('*',end=' ')
+    print()
+print("----------------")
+for i in range(5, 0, -1):
+    for j in range(5-i):
+        print(' ',end='')
+    for k in range(i):
+        print('*',end=' ')
+    print(' ')
